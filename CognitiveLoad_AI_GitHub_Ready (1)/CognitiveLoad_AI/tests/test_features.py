252602c95f5343cea15e235d60023e90b313728d
Ilma Rasheed <165518412+illmaahh@users.noreply.github.com>
2026-09-09 01:03:15 +0530

    Delete CognitiveLoad_AI_GitHub_Ready (1)/CognitiveLoad_AI directory

--- CognitiveLoad_AI_GitHub_Ready (1)/CognitiveLoad_AI/tests/test_features.py
+++
@@ -1,6 +0,0 @@
-from src.features import heuristic_load_score
-
-
-def test_score_bounds():
-    score = heuristic_load_score({"perf_reaction_time_s": 1.2, "perf_correct": 0.7})
-    assert 0 <= score <= 100
